838b7a854d2322901b38d0330887ca52b267422c
Yahsai <yahsaisantana1@gmail.com>
2024-04-26 21:22:25 -0400

    Update task 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-"""Measures the runtime of an async comprehension"""
-import asyncio
-import time
-wait_n = __import__('1-concurrent_coroutines').wait_n
+"""
+Measure the runtime
+"""
+from asyncio import run
+from time import time
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Measures the runtime of an async comprehension"""
-    start = time.time()
-    wait_n(n, max_delay)
-    end = time.time()
+    """
+    Measure the runtime
+    """
+    start = time()
+
+    run(wait_n(n, max_delay))
+
+    end = time()
+
     return (end - start) / n
