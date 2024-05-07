5dffe0f9335c4283e2e4e292e3c8bd3912651350
Yahsai <yahsaisantana1@gmail.com>
2024-05-07 00:47:57 -0400

    Task 11

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,9 @@
 export default function createEmployeesObject(departmentName, employees) {
+    if (typeof departmentName !== 'string' || !Array.isArray(employees)) {
+      throw new Error('Invalid arguments');
+    }
     return {
-      [departmentName]: employees,
+      [departmentName]: employees
     };
   }
+  
