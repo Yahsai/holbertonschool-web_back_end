03b255b1c49feb2c39f4b52ed47455c9c20941ad
Yahsai <yahsaisantana1@gmail.com>
2024-05-06 21:39:52 -0400

    Task 12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,8 +1,16 @@
 export default function createReportObject(employeesList) {
+    const allEmployees = {};
+    const departments = [];
+  
+    for (const [department, employees] of Object.entries(employeesList)) {
+      allEmployees[department] = employees;
+      departments.push(department);
+    }
+  
+    const getNumberOfDepartments = () => departments.length;
+  
     return {
-      allEmployees: { ...employeesList },
-      getNumberOfDepartments(employees) {
-        return Object.keys(employees).length;
-      },
+      allEmployees,
+      getNumberOfDepartments,
     };
   }
