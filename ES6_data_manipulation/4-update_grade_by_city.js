0118d9fe37da37a817065592ea7b20a092f1b97e
Yahsai <yahsaisantana1@gmail.com>
2024-05-23 13:02:39 -0400

    Task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,11 @@
-/* enlist-disable */
-export default function updateStudentGradeByCity(students, city, newGrades) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-  
-    const filter = students.filter((filteredObj) => filteredObj.location === city);
-    const map = filter.map((mappedObj) => {
-      const grade = newGrades.filter((newGradesObj) => newGradesObj.studentId === mappedObj.id);
-      const newObj = { ...mappedObj };
-      if (grade[0]) {
-        newObj.grade = grade[0].grade;
-      } else {
-        newObj.grade = 'N/A';
-      }
-      return newObj;
-    });
-    return map;
+export default function getStudentsByLocation(array, city, grad) {
+    return array
+      .filter((i) => i.location === city)
+      .map((student) => {
+        const gradeI = grad
+          .filter((i) => i.studentId === student.id)
+          .map((x) => x.grade)[0];
+        const grade = gradeI || 'N/A';
+        return { ...student, grade };
+      });
   }
