7cd75c09322952bd77fe8557270259d932da50dc
Yahsai <yahsaisantana1@gmail.com>
2024-10-13 19:55:34 -0400

    auth

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Doc
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+def remove_slash(string: str) -> str:
+    """
+    Funtion that removes forward slash
+    """
+    if string[-1] == '/':
+        string = string[:-1]
+    return string
+
+
+class Auth():
+    """ Doc """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Doc """
+        if not path or not excluded_paths or len(excluded_paths) == 0:
+            return True
+        excluded_path = [remove_slash(paths) for paths in excluded_paths]
+        if remove_slash(path) not in excluded_path:
+            return True
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Doc """
+        if request and 'Authorization' in request.headers.keys():
+            return request.headers['Authorization']
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Doc """
+        return None
