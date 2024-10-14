302e4deb833c7760434dbc11d6e85f784461e356
Yahsai <yahsaisantana1@gmail.com>
2024-10-13 21:50:06 -0400

    users

--- Basic_authentication/api/v1/views/__init__.py
+++ Basic_authentication/api/v1/views/__init__.py
@@ -1,11 +1,2 @@
-#!/usr/bin/env python3
-""" DocDocDocDocDocDoc
-"""
-from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-from api.v1.views.index import *
-from api.v1.views.users import *
-
-User.load_from_file()
+from api.v1.views.index import app_views  # Importa app_views desde index.py
+from api.v1.views.users import *  # Importa todas las rutas de users.py
--- Basic_authentication/api/v1/views/users.py
+++ Basic_authentication/api/v1/views/users.py
@@ -1,28 +1,25 @@
-#!/usr/bin/env python3
-""" Module of Users views
-"""
-from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
+from api.v1.views.index import app_views  # Importamos el app_views desde index.py
 
-
+# Ruta para obtener todos los usuarios
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
     Return:
-      - list of all User objects JSON represented
+      - List of all User objects in JSON format
     """
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
-
+# Ruta para obtener un usuario específico
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
     Path parameter:
-      - User ID
+      - user_id: ID of the User to retrieve
     Return:
-      - User object JSON represented
+      - JSON representation of the User object
       - 404 if the User ID doesn't exist
     """
     if user_id is None:
@@ -32,14 +29,14 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
     return jsonify(user.to_json())
 
-
+# Ruta para eliminar un usuario específico
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
     Path parameter:
-      - User ID
+      - user_id: ID of the User to delete
     Return:
-      - empty JSON is the User has been correctly deleted
+      - Empty JSON if the User has been deleted
       - 404 if the User ID doesn't exist
     """
     if user_id is None:
@@ -50,18 +47,18 @@ def delete_user(user_id: str = None) -> str:
     user.remove()
     return jsonify({}), 200
 
-
+# Ruta para crear un nuevo usuario
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user() -> str:
     """ POST /api/v1/users/
     JSON body:
       - email
       - password
-      - last_name (optional)
       - first_name (optional)
+      - last_name (optional)
     Return:
-      - User object JSON represented
-      - 400 if can't create the new User
+      - JSON representation of the created User object
+      - 400 if the User cannot be created
     """
     rj = None
     error_msg = None
@@ -88,19 +85,19 @@ def create_user() -> str:
             error_msg = "Can't create User: {}".format(e)
     return jsonify({'error': error_msg}), 400
 
-
+# Ruta para actualizar un usuario específico
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id: str = None) -> str:
     """ PUT /api/v1/users/:id
     Path parameter:
-      - User ID
+      - user_id: ID of the User to update
     JSON body:
-      - last_name (optional)
       - first_name (optional)
+      - last_name (optional)
     Return:
-      - User object JSON represented
+      - JSON representation of the updated User object
       - 404 if the User ID doesn't exist
-      - 400 if can't update the User
+      - 400 if the User cannot be updated
     """
     if user_id is None:
         abort(404)
