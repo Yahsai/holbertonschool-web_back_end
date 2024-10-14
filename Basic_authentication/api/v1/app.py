6c7a88e4905b012c32989fd0095f18f4e4afdbe8
Yahsai <yahsaisantana1@gmail.com>
2024-10-13 21:50:44 -0400

    app

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,18 +1,7 @@
-#!/usr/bin/env python3
-"""
-Route module for the API.
-
-Defines the protocol for every request,
-and deploys the correct type of authentication
-based on the environment variable 'AUTH_TYPE'.
-
-Also defines 3 URL paths and their responses.
-(read below)
-"""
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS
 import os
 
 from api.v1.auth.auth import Auth
@@ -24,33 +13,14 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth: Auth = None
-"""
-Should be an instance of a child class of 'Auth',
-depending on which authentication type is being
-run on this server,
 
-defined in the environment variable 'AUTH_TYPE'.
-"""
-# Fulfill sdabove docstring
 if os.environ.get("AUTH_TYPE", None) == "basic_auth":
-    # key may not exist.
-    # IF the key doesn't exist, we treat its value as None.
-    # and go with Auth.
     auth = BasicAuth()
 else:
     auth = Auth()
 
-
 @app.before_request
 def authenticate() -> None:
-    """
-    Before doing anything with a request,
-
-    This function
-    checks if the URL path the user is requesting
-    requires authorization. If it does, it validates the user's
-    credentials using Basic AUTH.
-    """
     if auth is None:
         return
 
@@ -63,40 +33,24 @@ def authenticate() -> None:
     AUTH_HEADER: str = auth.authorization_header(request)
 
     if AUTH_HEADER is None:
-        # User has not authorized themselves.
         abort(401)
-
-    # assert type(AUTH_HEADER) == str
 
     USER: User = auth.current_user(AUTH_HEADER)
 
     if USER is None:
-        # User doesn't have valid credentials.
         abort(403)
-
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """
-    Unauthorized (401) error handler
-    """
     return jsonify({"error": "Unauthorized"}), 401
-
 
 @app.errorhandler(403)
 def unauthorized(error) -> str:
-    """
-    Forbidden (403) error handler
-    """
     return jsonify({"error": "Forbidden"}), 403
-
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
     return jsonify({"error": "Not found"}), 404
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
