b510d7d5dbdd8e10616e18d8b3c1da00a48baec7
Yahsai <yahsaisantana1@gmail.com>
2024-10-27 23:34:47 -0400

    Update 2-app.py

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,48 +1,39 @@
 #!/usr/bin/env python3
-"""
-Exercise 2: Use the 'babel.localeselector'
-to find the best matching available language
-to give to the user.
-"""
-import flask
-import flask_babel
-from typing import Optional
-from os import environ
+""" Route module for the API - Get locale from request"""
 
 
-class Config:
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from os import getenv
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Setup - Babel configuration """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object('2-app.Config')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return: 2-index.html
     """
-    Configuration class for Babel and allowed languages.
-    Contains the allowed languages and default timezone.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    return render_template('2-index.html')
 
 
-app = flask.Flask(__name__)
-app.config.from_object(Config)
-babel = flask_babel.Babel(app)
-
-
-@babel.localeselector
-def get_locale() -> Optional[str]:
-    """
-    Select the best match for supported languages from the request.
-    """
-    return flask.request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route("/", strict_slashes=False)
-def home() -> str:
-    """
-    Render the home page template with localized content.
-    """
-    return flask.render_template("2-index.html")
+def get_locale() -> str:
+    """ Determines best match for supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    # Fallback to '0.0.0.0' and port 5000 if no HOST/PORT env vars are defined
-    host = environ.get("HOST", "0.0.0.0")
-    port = int(environ.get("PORT", 5000))
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
