fb04a40be6257fb76760c8af1db0617e8f3a2194
Yahsai <yahsaisantana1@gmail.com>
2024-10-27 23:57:45 -0400

    Task 4

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -7,13 +7,16 @@ from os import getenv
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
     """ Setup - Babel configuration """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-app.config.from_object(Config)  # Adjusted to directly reference Config class
+
+app.config.from_object('4-app.Config')
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
@@ -22,13 +25,16 @@ def index() -> str:
     """
     return render_template('4-index.html')
 
-@babel.localeselector  # Added decorator
+
 def get_locale() -> str:
     """ Determines best match for supported languages """
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    if request.args.get('locale'):
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
